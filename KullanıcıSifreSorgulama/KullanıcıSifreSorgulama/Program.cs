ec9bdf914a81b3a9a781d9249aedce6404a2e23b
yigitcicekk <ygtcck.67@gmail.com>
2022-10-23 20:05:54 +0300

    KullanıcıSifreSorgulama
    Kullanıcıdan girilen kullanıcı adı ve şifreyi sistemde kayıtlı olan kullanıcı adı veya şifre mi diye kontrol eder

---
+++ KullanıcıSifreSorgulama/KullanıcıSifreSorgulama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KullanıcıSifreSorgulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kullanıcı adı giriniz :");
+            string kullanıcıadi = Console.ReadLine();
+
+            Console.Write("Şifrenizi giriniz :");
+            string sifre = Console.ReadLine();
+
+            if (kullanıcıadi == "admin" && sifre == "123")
+            {
+                Console.WriteLine("Merhaba, Başarılı giriş yaptınız");
+            }
+            else
+            {
+                Console.WriteLine("Girişiniz Başarısız");
+            }
+            Console.ReadLine();
+        }
+    }
+}
